3d105261fea60c0b0156975e8564c6e486c3d004
AppelJoosh <joshcauson@gmail.com>
2026-01-12 04:01:12 +1100

    realised something simpler already existed Oops

--- scenes/microgames/avoid/avoid.gd
+++ scenes/microgames/avoid/avoid.gd
@@ -8,6 +8,7 @@ var player_movement = Vector2()
 
 func _ready() -> void:
 	super()
+	blurb = "Avoid!"
 	win_on_timeout = true
 	var hazard_count = 2 + floor(speed_factor * 0.12)
 	for i in range(hazard_count):
